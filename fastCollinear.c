80ba72ef245940fc1e281ea650306175df830e94
Neebum <68243658+Neebum@users.noreply.github.com>
2020-07-26 11:33:57 +0530

    Update fastCollinear.c

--- fastCollinear.c
+++ fastCollinear.c
@@ -10,15 +10,17 @@ typedef struct point_ {
     int y;
 }point;
 
-point* compareTo(point *p1,point *p2) {
-    if(p1->y<p2->y) return p1;
+int compareTo(const void *pp1,const void *pp2) {
+    point *p1 = (point *) pp1;
+    point *p2 = (point *) pp2;
+    if(p1->y<p2->y) return -1;
     else if(p1->y==p2->y && p1->x<p2->x)
-    return p1;
-    else return p2;
+    return -1;
+    else return 1;
 }
 float slope(point *p1,point *p2) {
     if(p2->y==p1->y && p2->x!=p1->x) return 0;//horizontal line segment
-   else if(p2->x==p1->x && p2->y!=p1->y) return inf; //vertical line segment
+   else if(p2->x==p1->x && p2->y!=p1->y) return inf; //verticl line segment
    else if(p2->x==p1->x && p2->y==p1->y) return -inf;//point to itself
    else 
    return (p2->y-p1->y)/((float)(p2->x-p1->x));
@@ -41,18 +43,47 @@ bool collinear(point *p,point *p1,point *p2,point *p3){
 void display (point *p){
 	printf("(%d, %d)",p->x,p->y);
 }
-int fastCollinearPoints(point *p,int n) {
+int fastCollinearPoints(point *p,int n)  {
     point copy[n];
     int i,j;
     for(i=0;i<n;i++) copy[i]=p[i];
+     int numOfSeg=0;
+     qsort(copy,n,sizeof(point),compareTo);
+     /*for(int k=0;k<n;k++){ //tp display sorted copy as per points
+        printf("\t");
+        display(&copy[k]);
+    }
+    printf("\n");*/
     for(i=0;i<n;i++){
-    qsort_r(copy,n,sizeof(point),slopeOrder,&copy[0]);
-       for(j=1;j<i+1;j++){//ignoring copy[0] as slope with itself will be smallest
-           if(slope(&p[i],&copy[]))
+    qsort_r(copy,n,sizeof(point),slopeOrder,&copy[i]);
+    
+    float currSlope=slope(&p[i],&copy[1]);
+    float prevSlope=currSlope;
+    int count=0;
+       for(j=1;j<n-i;j++){//ignoring copy[0] as slope with itself will be smallest
+           if(slope(&p[i],&copy[j])==currSlope){
+               count++;
+           }
+           //counting from new set of slopes
+           else { 
+           //to display line segment that has more than 3 collinear points
+               if(count>=3) { 
+                   display(&p[i]); printf("-> "); display(&copy[j-1]); 
+                   printf("\n");
+               }
+                count=0;
+               
+               currSlope = slope(&p[i],&copy[j]);
+               prevSlope = currSlope;
+           }
+           if(count>=3) {
+              if(numOfSeg==0) numOfSeg++; //for first set of line segments
+              if(prevSlope!=currSlope) numOfSeg;  //for other sets of collinears
+           }
        } 
     }
-   
 }
+
 int main () {
     
     int i,size;
@@ -63,6 +94,7 @@ int main () {
         printf("Enter x and y for point-%d :\n",i+1);
         scanf("%d %d",&p[i].x,&p[i].y);    
     }
+    fastCollinearPoints(p,size);
 
     return 0;
 }
