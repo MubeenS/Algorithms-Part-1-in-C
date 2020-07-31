69e1c08cb6dbbc56476727061804f0241fc01830
Neebum <68243658+Neebum@users.noreply.github.com>
2020-07-31 16:37:40 +0530

    added functions

--- puzzle8.c
+++ puzzle8.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 //input 3 0 1 3 4 2 5 7 8 6 
+//input 3 8 1 3 4 0 2 7 6 5
 
 int **tiles;
 int **goal;
@@ -37,22 +38,21 @@ void createGoal(void) {
     int filler=1;
     for(i=0;i<N;i++) {
     	for(j=0;j<N;j++) {
-    		if(i==N-1 && j==N-1) {
+    		if(i==N-1 && j==N-1) 
     			goal[i][j] = 0;
-			}
 			else
     		goal[i][j] = filler++;
 		}
 	}
 }
-void display(int **tiles) {
+void display(int **t) {
 	int i,j;
 	printf("%d\n",N);
 	 for(i=0;i<N;i++) {
     	for(j=0;j<N;j++) {
-    		if(tiles[i][j]==0) printf("  ");
+    		if(t[i][j]==0) printf("  ");
     		else
-    		printf("%d ",tiles[i][j]);
+    		printf("%d ",t[i][j]);
 		}
 		printf("\n");
 	}
@@ -63,6 +63,36 @@ int hamming(void) {
     	for(j=0;j<N;j++) 
     		if(tiles[i][j]!=goal[i][j]) count++;
     		return count;
+}
+
+int manhattan(void) {
+	int idx=0,hash[N*N],i,j,ir,jr,dist=0;
+	for(i=0;i<N;i++) 
+	 for(j=0;j<N;j++){
+	 	if(i==N-1 && j==N-1) 
+    			hash[idx] = 0;
+			else
+    		hash[idx++] = i*10+j;
+	 }
+	 for(i=0;i<N*N;i++)
+	 printf("%d ",hash[i]);
+	 
+	 for(i=0;i<N;i++)
+	  for(j=0;j<N;j++) {
+	  	// taking out actual i and j that should be
+	  	ir=hash[tiles[i][j]] / 10;
+	  	jr=hash[tiles[i][j]] % 10;
+	  	dist = dist + abs(ir-i) + abs(jr-j);
+	  }
+	  return dist;
+}
+
+bool isGoal(int **t) {
+	int i,j;
+	 for(i=0;i<N;i++) 
+    	for(j=0;j<N;j++) 
+    		if(t[i][j]!=goal[i][j]) return false;
+	return true;
 }
 
 int main () {
@@ -85,6 +115,12 @@ int main () {
     printf("\n");
     display(goal);
     int ham = hamming();
-    printf("%d",ham);
+    printf("hamming = %d\n",ham);
+    int man= manhattan();
+    printf("Manhattan = %d\n",man);
+    bool x= isGoal(tiles);
+    printf("%s  ", x ? "true" : "false");
+    x= isGoal(goal);
+    printf("%s", x ? "true" : "false"); 
 	return 0;
 }
